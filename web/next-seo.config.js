20b1094deae8d9666e8f0207def1e9312eb8df56
Ayresia <nalizyoncatania0@gmail.com>
2022-02-03 16:07:43 +0100

    Add frontend meta tags

---
+++ web/next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+    title: "Linkly",
+    description: "Shorten your links, on your own terms.",
+    openGraph: {
+        type: 'website',
+        locale: 'en_GB',
+        site_name: 'Linkly',
+        description: "Shorten your links, on your own terms.",
+    },
+    twitter: {
+        cardType: 'summary_large_image',
+    }
+}
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,15 @@
+import { DefaultSeo } from 'next-seo';
 import App, { AppContext } from 'next/app'
+import SEO from '../next-seo.config';
 import '../styles/globals.css'
 
 export default function LinklyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <DefaultSeo {...SEO} />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 LinklyApp.getInitialProps = async (ctx: AppContext) => {
